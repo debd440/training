16263d387c999c85362bd0cb1bd80eaf41445b1d
Debdutta Bhattacharya <deb@jojodelivery.com>
2022-09-05 20:02:51 +0530

    Updated New Probs

--- DSTutorials/src/main/java/com/deb/ds/core/tree/BinarySearchTree.java
+++ DSTutorials/src/main/java/com/deb/ds/core/tree/BinarySearchTree.java
@@ -2,4 +2,27 @@ package com.deb.ds.core.tree;
 
 public class BinarySearchTree {
 
+	// Diameter of tree Or Max length Of Tree
+	// 10
+	// 11 15
+	// 32 25 17 9
+	public static void main(String[] args) {
+
+		BinarySearchTree demo = new BinarySearchTree();
+
+		TreeNode node = TreeNode.createBinaryTree();
+
+		System.out.println("Height: " + demo.findHeight(node));
+	}
+
+	private int findHeight(TreeNode node) {
+
+		if (node == null)
+			return 0;
+
+		int leftHeight = findHeight(node.left);
+		int rightHeight = findHeight(node.right);
+
+		return Math.max(leftHeight, rightHeight) + 1;
+	}
 }
