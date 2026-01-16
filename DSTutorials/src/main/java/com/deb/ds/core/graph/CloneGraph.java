b89ab33d53c07dbfe07be625bd6908df97f345ec
debd440 <debdutta.bhattacharya@yahoo.com>
2026-01-16 23:25:20 +0530

    Added Some details

--- DSTutorials/src/main/java/com/deb/ds/core/graph/CloneGraph.java
+++ DSTutorials/src/main/java/com/deb/ds/core/graph/CloneGraph.java
@@ -1,27 +1,48 @@
 package com.deb.ds.core.graph;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class CloneGraph {
 
-	class Node {
-		String data;
-		List<Node> neighbours;
-	}
+    public static void main(String[] args) {
+        Map<Node, Node> map = new HashMap<>();
+        Node node1 = new Node("A");
+        Node node2 = new Node("B");
+        Node node3 = new Node("C");
+        node1.neighbours.add(node1);
+        node2.neighbours.add(node3);
+        node3.neighbours.add(node1); // cy
+        CloneGraph cg = new CloneGraph();
+        Node clonedGraph = cg.cloneGraph(node1, map);
 
-	public Node cloneGraph(Node head, Map<Node, Node> map) {
-		if (head == null)
-			return null;
-		if (map.containsKey(head))
-			return map.get(head);
+        System.out.println("Graph cloned successfully.");
+    }
 
-		Node current = new Node();
-		current.data = head.data;
+    public Node cloneGraph(Node head, Map<Node, Node> map) {
+        if (head == null)
+            return null;
+        if (map.containsKey(head))
+            return map.get(head);
 
-		for (Node neighbour : head.neighbours) {
-			current.neighbours.add(cloneGraph(neighbour, map));
-		}
-		return current;
-	}
+        Node clone = new Node(head.data);
+        map.put(head, clone);
+
+        for (Node neighbour : head.neighbours) {
+            clone.neighbours.add(cloneGraph(neighbour, map));
+        }
+        return clone;
+    }
+}
+
+class Node {
+    String data;
+    List<Node> neighbours;
+
+    public Node(String data) {
+        this.data = data;
+        this.neighbours = new ArrayList<>();
+    }
 }
--- DSTutorials/src/main/java/com/deb/ds/core/gready/ActivitySelectionProblem.java
+++ DSTutorials/src/main/java/com/deb/ds/core/gready/ActivitySelectionProblem.java
@@ -1,5 +1,28 @@
 package com.deb.ds.core.gready;
 
+import java.util.Arrays;
+
 public class ActivitySelectionProblem {
 
+    public static void main(String[] args) {
+        int[] start = {1, 3, 0, 5, 8, 5};
+        int[] finish = {2, 4, 6, 7, 9, 9};
+
+        System.out.println(maxActivities(start, finish)); // O
+    }
+
+    private static int maxActivities(int start[], int end[]) {
+        Arrays.sort(start);
+        Arrays.sort(end);
+        int i = 0;
+        int count = 0;
+        for (int j = 1; j < start.length - 1; j++) {
+            if (start[i] < end[j]) {
+                count++;
+            } else {
+                i = j;
+            }
+        }
+        return count;
+    }
 }
